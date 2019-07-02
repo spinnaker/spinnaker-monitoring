a37ddcef19500350bf48fb2a9ae94f24c26e8e81
Miles Matthias <mmatthias@google.com>
2019-07-02 17:22:03 -0600

    improve prometheus operator setup script (#211)
    * refactor(prometheus-operator): improve prometheus operator setup script
    
    * style(monitoring): tweaks per review comment

--- spinnaker-monitoring-third-party/third_party/prometheus_operator/setup.sh
+++ spinnaker-monitoring-third-party/third_party/prometheus_operator/setup.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONTEXT=${1:-$(kubectl config current-context)}
+CONTEXT=$(kubectl config current-context)
+NAMESPACE=${1:-default}
+KUBECTL="kubectl --context $CONTEXT --namespace $NAMESPACE"
+echo "Using context $CONTEXT and namespace $NAMESPACE."
 
-
-echo "Adding the Spinnaker ServiceMonitor to current context: ${CONTEXT}"
-
-kubectl apply -f spinnaker-service-monitor.yaml
-
+echo "Adding the Spinnaker ServiceMonitor..."
+$KUBECTL apply -f spinnaker-service-monitor.yaml
 
 echo "Generating Grafana dashboard configmaps..."
-
-for filename in $ROOT/../prometheus/*-dashboard.json; do
-  fn_only=$(basename $filename)
+for filename in "$ROOT"/../prometheus/*-dashboard.json; do
+  fn_only=$(basename "$filename")
   fn_root="${fn_only%.*}"
   dest_file="generated_dashboards/${fn_root}.yaml"
 
-  cat grafana-dashboard.yaml.template | sed -e "s/%DASHBOARD%/${fn_root}/" > $dest_file
-  printf "  ${fn_only}: |-\n" >> $dest_file
+  cat grafana-dashboard.yaml.template | sed -e "s/%DASHBOARD%/$fn_root/" > $dest_file
+  printf "  $fn_only: |-\n" >> $dest_file
 
   cat $filename | sed -e "/\"__inputs\"/,/],/d" \
       -e "/\"__requires\"/,/],/d" \
@@ -26,7 +25,5 @@ for filename in $ROOT/../prometheus/*-dashboard.json; do
   >> $dest_file
 done
 
-
-echo "applying dashboards as configmaps to cluster..."
-
-kubectl apply -f generated_dashboards
+echo "Applying dashboards as configmaps to cluster..."
+$KUBECTL apply -f generated_dashboards
